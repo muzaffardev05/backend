3a17ba99eee976d5ada4b3f6243d6b1fa0e0ca1e
Muzaffar Hussain <muzaffarhussain0055@gmail.com>
2026-07-09 11:17:10 +0500

    created data folder

---
+++ app/data/locations.py
@@ -0,0 +1,45 @@
+LOCATIONS = {
+    # Cities
+    "karachi": ["karachi","khi"],
+    "lahore": ["lahore","lhr"],
+    "islamabad": ["islamabad", "isb", "islamabad capital territory"],
+    "rawalpindi": ["rawalpindi", "pindi"],
+    "faisalabad": ["faisalabad"],
+    "multan": ["multan"],
+    "hyderabad": ["hyderabad", "hyd"],
+    "peshawar": ["peshawar"],
+    "quetta": ["quetta"],
+    "gujranwala": ["gujranwala"],
+    "sialkot": ["sialkot"],
+    "bahawalpur": ["bahawalpur"],
+    "sukkur": ["sukkur"],
+    "gwadar": ["gwadar"],
+    "abbottabad": ["abbottabad"],
+    "mirpur": ["mirpur"],
+    "gilgit": ["gilgit"],
+
+    # Provinces
+    "punjab": ["punjab"],
+    "sindh": ["sindh"],
+    "kpk": [
+        "kpk",
+        "kp",
+        "khyber pakhtunkhwa",
+        "nwfp"
+    ],
+    "balochistan": [
+        "balochistan",
+        "baloch"
+    ],
+    "ajk": [
+        "ajk",
+        "azad kashmir"
+    ],
+    "gilgit baltistan": [
+        "gilgit baltistan",
+        "gb"
+    ],
+
+    # Country
+    "pakistan": ["pakistan"]
+}
